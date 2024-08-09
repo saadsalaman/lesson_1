eee6a99dda31101026f065de04f81ff38b3018dd
Syed Muhammad Saad Salman <im.saadsalmanhashmi@gmail.com>
2024-08-09 10:43:02 +0500

    my initial commit in sub branch

--- calcualtor.py
+++ calcualtor.py
@@ -1,6 +1,15 @@
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a*b
+
 if __name__== "__main__":
     result = add(3, 4)
+    subtract_result = subtract(4, 3)
     print(f"Result: {result}")
-    
+    print(f"Subtract: {subtract_result}")    
+
